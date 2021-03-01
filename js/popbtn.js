b4838c45eaa20421f745a7b09897dfc131fbbf96
enkoding1 <ksjdevelop53@gmail.com>
2021-03-02 03:18:00 +0900

    add

--- js/popbtn.js
+++ js/popbtn.js
@@ -1,16 +1,27 @@
 var wide = document.querySelector('.pop-btn');
 var mylist = document.querySelector("#mylist");
-
+var Body_1 = document.querySelector('body');
 
 wide.addEventListener("mouseover",mouseOver);
 wide.addEventListener("mouseout",mouseOut);
-
+Body_1.addEventListener("click",onClick);
 
 
 function mouseOver(){
     mylist.style.display = "block";
 }
 function mouseOut(){
-   mylist.style.display = "none";
+    mylist.style.display = "none";
+} 
+function onClick(){
+    mylist.style.display = "none";
 }
 
+// document.addEventListener("scroll",function(){
+//    var currentScroll = document.documentElement.scrollTop;
+//    console.log("current scroll: "+ currentScroll);
+//    if(currentScroll == true){
+//        mylist.style.display = "none";
+//    }
+  
+// })
