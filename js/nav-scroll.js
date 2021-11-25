8f766631554d39356043e3a4bc1a026ec71b765f
enKODING <65354945+enKODING1@users.noreply.github.com>
2021-11-26 02:05:15 +0900

    change:mobile navbar

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -7,7 +7,7 @@ window.document.onmousewheel = (e)=>{
     wheel += e.deltaY * -0.01;
     wheel = Math.min(Math.max(wheel,-2.5),0);
     if(wheel == 0){
-        navbar.style.display = "block";
+      
         navbar.style.transform = `translate(0px,0px)`;
     }else if(wheel == -2.5){
         navbar.style.transform = `translate(0px,-60px)`;
