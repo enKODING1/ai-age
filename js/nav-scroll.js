41d9da13bdd3bbd16c89e4642ce6d55d5bcc276e
enkoding1 <ksjdevelop53@gmail.com>
2021-11-27 00:29:34 +0900

    fix:nav-scroll.js

--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,4 +1,4 @@
-const navbar = document.querySelector(".nav-bar");
+const nav_bar = document.querySelector(".nav-bar");
 let wheel = 0;
 
 
@@ -7,11 +7,8 @@ window.document.onmousewheel = (e)=>{
     wheel += e.deltaY * -0.01;
     wheel = Math.min(Math.max(wheel,-2.5),0);
     if(wheel == 0){
-      
-        navbar.style.transform = `translate(0px,0px)`;
+        nav_bar.style.transform = `translate(0px,0px)`;
     }else if(wheel == -2.5){
-        navbar.style.transform = `translate(0px,-60px)`;
-       
+        nav_bar.style.transform = `translate(0px,-60px)`;
     }
-
 }
